2092abf8a1b2af5af59504daffb2bb0ea833475a
khauminhduy <khauminhduy@gmail.com>
2021-06-01 16:23:30 +0700

    update

--- src/main/java/com/example/demo/controller/admin/OrderManagerController.java
+++ src/main/java/com/example/demo/controller/admin/OrderManagerController.java
@@ -20,39 +20,37 @@ public class OrderManagerController {
 
 	@Autowired
 	private OrderService orderService;
-	
+
 	@Autowired
 	private AccountService accountService;
-	
-    @GetMapping("/order-manager")
-    public String getOrderManager(Model model) {
-    	List<Order> orders = orderService.getAllLists();
-    	model.addAttribute("orders", orders);
-    
-    	
-        return "admin/ordermanager";
-    }
-    
-    @GetMapping("/edit-order")
-    public String editOrder(@RequestParam("id") String id, Model model) {
-    	Order order = orderService.getOrder(Long.parseLong(id));
-    	model.addAttribute("order", order);
-    	return "admin/editorder";
-    }
-    
-    @GetMapping("/delete-order")
-    public String deleteOrder(@RequestParam("id") String id) {
-    	orderService.deleteOrder(Long.parseLong(id));
-    	return "redirect:/order-manager";
-    }
-    
-    @PostMapping("/save-order")
-    public String saveOrder(@ModelAttribute("order") Order order, @RequestParam("accountUsername") String accountUsername) {
-    	order.setOrderDate(LocalDateTime.now());
-    	order.setAccount(accountService.findByUsername(accountUsername));
-    	orderService.save(order);
-    	return "redirect:/order-manager";
-    }
-    
-    
+
+	@GetMapping("/order-manager")
+	public String getOrderManager(Model model) {
+		List<Order> orders = orderService.getAllLists();
+		model.addAttribute("orders", orders);
+		return "admin/ordermanager";
+	}
+
+	@GetMapping("/edit-order")
+	public String editOrder(@RequestParam("id") String id, Model model) {
+		Order order = orderService.getOrder(Long.parseLong(id));
+		model.addAttribute("order", order);
+		return "admin/editorder";
+	}
+
+	@GetMapping("/delete-order")
+	public String deleteOrder(@RequestParam("id") String id) {
+		orderService.deleteOrder(Long.parseLong(id));
+		return "redirect:/order-manager";
+	}
+
+	@PostMapping("/save-order")
+	public String saveOrder(@ModelAttribute("order") Order order,
+			@RequestParam("accountUsername") String accountUsername) {
+		order.setOrderDate(LocalDateTime.now());
+		order.setAccount(accountService.findByUsername(accountUsername));
+		orderService.save(order);
+		return "redirect:/order-manager";
+	}
+
 }
